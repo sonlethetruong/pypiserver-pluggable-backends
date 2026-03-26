92ffd77c4d79cf2c366239077e4cd724e4ff2a8f
sonlethetruong <53509390+sonlethetruong@users.noreply.github.com>
2026-03-26 11:01:56 +0700

    Implement server base URL handling in main app
    Added logic to handle server base URL for mounting.

--- pypiserver/__main__.py
+++ pypiserver/__main__.py
@@ -199,6 +199,13 @@ def main(argv: t.Sequence[str] = None) -> None:
             "Running bottle with selected server '%s'", config.server_method
         )
 
+    # We strip&check because bottle mount() will raise error if path has no segment.
+    if config.server_base_url.strip("/"):
+        main_app = bottle.Bottle()
+        main_app.mount(config.server_base_url, app)
+    else:
+        main_app = app
+
     bottle.run(
         app=app,
         host=config.host,
